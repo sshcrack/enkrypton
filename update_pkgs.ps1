3ca36d3c95ccfd9fac968ac26d472a8c9f0c6136
Hendrik Lind <34072808+sshcrack@users.noreply.github.com>
2024-02-12 14:37:19 +0100

    updated packages

--- update_pkgs.ps1
+++ update_pkgs.ps1
@@ -1,8 +1,11 @@
 $configDirs = @("src-packages", "src-tauri") | Get-ChildItem -Exclude target -Recurse -Filter Cargo.toml -Depth 2 | ForEach-Object { $_.Directory.FullName }
+Write-Host "Updating index..."
+cargo install empty-library 2> $NULL
 foreach ($dir in $configDirs) {
     Write-Host "Updating packages in $dir"
     Push-Location $dir
-    cargo upgrade -i
     cargo update
     Pop-Location
 }
+
+Write-Host "Dependencies updated."
